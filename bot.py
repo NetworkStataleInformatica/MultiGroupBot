9dbe90a1b82a5484ed110d66e6e4f17063245e0d
Giuseppetm <gdeambo@yahoo.it>
2020-04-23 21:41:15 +0200

    Added code di dennis farina

---
+++ bot.py
@@ -0,0 +1,13 @@
+from telegram.ext import Updater, CommandHandler
+
+def hello(update, context):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+
+updater = Updater('893851442:AAEtZPUJQbHKPlrRAORI6AtEmd7Nf6G_4eU', use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
